0a9a7e766a6f118a76ba073ff8e448f513587bdc
Prayag Sahu <sahuprayag229@gmail.com>
2025-12-27 16:28:13 +0530

    script

--- 01_colorChanger/script.js
+++ 01_colorChanger/script.js
@@ -1,7 +1,6 @@
 const buttons = document.querySelectorAll('.button');
 const body = document.querySelector('body');
 
-
 buttons.forEach(function (button) {
     console.log(button);
     button.addEventListener('click', function (e) {

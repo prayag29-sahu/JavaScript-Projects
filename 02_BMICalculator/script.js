60207dd34b41073f9d66d749627a08262baf3e2f
Prayag Sahu <sahuprayag229@gmail.com>
2025-12-28 11:52:11 +0530

    script logic

--- 02_BMICalculator/script.js
+++ 02_BMICalculator/script.js
@@ -1,5 +1,6 @@
 const form = document.querySelector('form');
-
+// this usecase will give you empty
+// const height = parseInt(document.querySelector('#height').value)
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();

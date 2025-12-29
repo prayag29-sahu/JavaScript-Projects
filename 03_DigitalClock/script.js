a7d2d59b068f9fce41ada0bf63dc5c718cee5913
Prayag Sahu <sahuprayag229@gmail.com>
2025-12-29 13:25:33 +0530

    js logic

--- 03_DigitalClock/script.js
+++ 03_DigitalClock/script.js
@@ -1,5 +1,6 @@
 const clock = document.getElementById('clock');
 // const clock = document.querySelector('#clock')
+
 setInterval(function () {
     let date = new Date();
     // console.log(date.toLocaleTimeString());
